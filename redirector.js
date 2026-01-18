427619d11a71a9e0eecef41593f3ec3a737a7706
ThisAMJ <69196954+ThisAMJ@users.noreply.github.com>
2026-01-18 23:05:43 +1100

    initial commit

---
+++ redirector.js
@@ -0,0 +1,12 @@
+const PORT = 80;
+
+function handler(req) {
+    const url = new URL(req.url);
+    let pathname = url.pathname;
+    if (pathname.startsWith('/')) {
+        pathname = pathname.slice(1);
+    }
+    return new Response(null, { status: 302, headers: { Location: `https://autorender.p2sr.org/${pathname}` } });return new Response(null, { status: 302, headers: { Location: "https://portal2.sr/" } });
+}
+
+Deno.serve({ port: PORT }, handler);
