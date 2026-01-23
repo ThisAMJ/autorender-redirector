55a0e7abac97955297421c193a456ffb355b937d
ThisAMJ <69196954+ThisAMJ@users.noreply.github.com>
2026-01-23 18:23:31 +1100

    fix: pass through search params

--- redirector.js
+++ redirector.js
@@ -2,11 +2,7 @@ const PORT = 80;
 
 function handler(req) {
     const url = new URL(req.url);
-    let pathname = url.pathname;
-    if (pathname.startsWith('/')) {
-        pathname = pathname.slice(1);
-    }
-    return new Response(null, { status: 302, headers: { Location: `https://autorender.p2sr.org/${pathname}` } });
+    return new Response(null, { status: 302, headers: { Location: `https://autorender.p2sr.org${url.pathname}${url.search}` } });
 }
 
 Deno.serve({ port: PORT }, handler);
